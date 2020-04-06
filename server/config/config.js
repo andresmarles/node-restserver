0d970c6e54f2d4781f6dc74a7a3331ce32c30e19
Andres Marles <andres0259@hotmail.com>
2020-04-06 00:12:45 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,8 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
    urlDB = "mongodb://localhost:27017/cafe";
 } else {
-   urlDB =
-      "mongodb+srv://andres:d9BEL73KcQF58Ntf@cluster0-ag1wq.mongodb.net/cafe";
+   urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
