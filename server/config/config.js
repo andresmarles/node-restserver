2a4ea302aa4ebc124401baf70393800b4a5e0790
Andres Marles <andres0259@hotmail.com>
2020-04-05 22:58:23 -0500

    engines:{node. 13.3.0}

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 // ============================
 let urlDB;
 
-if (process.env.NODE_ENV || "dev") {
+if (process.env.NODE_ENV === "dev") {
    urlDB = "mongodb://localhost:27017/cafe";
 } else {
    urlDB =
