990649a1b0d56bbe6874969d1809435b5df00206
Andres Marles <andres0259@hotmail.com>
2020-04-05 20:57:35 -0500

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-//===============
-// Puerto
-//===============
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV || "dev") {
+   urlDB = "mongodb://localhost:27017/cafe";
+} else {
+   urlDB =
+      "mongodb+srv://andres:d9BEL73KcQF58Ntf@cluster0-ag1wq.mongodb.net/cafe";
+}
+
+process.env.urlDB = urlDB;
