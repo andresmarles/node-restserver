ee095301a5906a8a024ba437006c582fc3820771
Andres Marles <andres0259@hotmail.com>
2020-04-05 21:10:21 -0500

    Arreglo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 require("./config/config");
 
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
-
 const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
@@ -13,18 +10,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require("./routes/usuario"));
+app.get("/usuario", function (req, res) {
+   res.json("get usuario");
+});
 
-mongoose.connect(
-   process.env.urlDB,
-   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-   (err, res) => {
-      if (err) throw err;
+app.post("/usuario", function (req, res) {
+   let body = req.body;
 
-      console.log("base de datos Online");
+   if (body.nombre === undefined) {
+      res.status(400).json({
+         ok: false,
+         mensaje: "El numero es necesario",
+      });
+   } else {
+      res.json({ persona: body });
    }
-);
+});
+
+app.put("/usuario/:id", function (req, res) {
+   let id = req.params.id;
+   res.json({ id });
+});
+
+app.delete("/usuario", function (req, res) {
+   res.json("delete usuario");
+});
 
 app.listen(process.env.PORT, () => {
-   console.log("Escuchando puerto: ", process.env.PORT);
+   console.log("Escuchando puerto : ", process.env.PORT);
 });
